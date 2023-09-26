d91f0e7d599e1a821df9a0bd7e607161f556226b
Hoangnhan1708 <truongnhan115@gmail.com>
2023-09-26 22:54:55 +0700

    second commit

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var isShowHeaderDownload = false;
 var isShowCustomerCare = false;
 var isShowCheckOrder = false;
 var isShowChangeLanguage = false;
+var isShowHistorySearch = false;
 
 // Handle show/hide header download
 $(".js-header__download-btn").onclick = function() {
@@ -55,6 +56,17 @@ $(".js-header__changeLanguage-btn").onclick = function() {
     }
 };
 
+$(".header__search-input").onfocus = function(){
+    if(!isShowHistorySearch){
+        $('.header__search-history').classList.add('open');
+        isShowHistorySearch = true
+    }
+    else{
+        $('.header__search-history').classList.remove('open');
+        isShowHistorySearch = false
+    }
+}
+
 
 window.onclick = function(event) {
     if(isShowHeaderDownload && event.target !== $('.js-header__download-btn') && event.target !== $('.js-header__download')){
@@ -76,7 +88,14 @@ window.onclick = function(event) {
         $('.js-header__changeLanguage').classList.remove('open');
         isShowChangeLanguage = false
     }
+
+    if(isShowHistorySearch && event.target !== $(".header__search-input") && event.target !== $('.header__search-history')){
+        $('.header__search-history').classList.remove('open');
+        isShowHistorySearch = false
+    }
 };
+
+// Stop pagination
 $('.js-header__download').addEventListener('click',function(event){
     event.stopPropagation()
 })
@@ -92,3 +111,55 @@ $('.js-header__checkOrder').addEventListener('click',function(event){
 $('.js-header__changeLanguage').addEventListener('click',function(event){
     event.stopPropagation()
 })
+
+$('.header__search-history').addEventListener('click',function(event){
+    event.stopPropagation()
+})
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = $(".header-content");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slider-item");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
